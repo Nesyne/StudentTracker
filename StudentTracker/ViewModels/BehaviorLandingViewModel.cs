5caf8da4323bee003124f6274b841b14884fc325
Nick <sphycx@gmail.com>
2018-12-29 23:55:09 -0800

    First forms

---
+++ StudentTracker/ViewModels/BehaviorLandingViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+namespace StudentTracker.ViewModels
+{
+    public class BehaviorLandingViewModel:BaseViewModel
+    {
+        public BehaviorLandingViewModel()
+        {
+            Title = "Behavior/Evaluation";
+        }
+    }
+}
---
+++ StudentTracker/Views/BehaviorLandingPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using StudentTracker.ViewModels;
+using Xamarin.Forms;
+
+namespace StudentTracker.Views
+{
+    public partial class BehaviorLandingPage : ContentPage
+    {
+        BehaviorLandingViewModel _viewModel;
+
+        public BehaviorLandingPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new BehaviorLandingViewModel();
+        }
+    }
+}
