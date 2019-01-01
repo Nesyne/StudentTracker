478cf8aba98023e9c95a22797025663e3d5def4c
Nick <sphycx@gmail.com>
2018-12-31 21:13:52 -0800

    more changes

---
+++ StudentTracker/Views/BehaviorPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace StudentTracker.Views
+{
+    public partial class BehaviorPage : ContentPage
+    {
+        public BehaviorPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
